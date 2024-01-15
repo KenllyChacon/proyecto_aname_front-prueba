76b9e95ef9ab1102c4bfbc0ca4431d2a01947108
dorisvchb <100151176+dorisvchb@users.noreply.github.com>
2024-01-15 04:02:44 -0500

    Estilo pag Crear Campeonato

--- src/pages/Negocio/CrearCampeonato.vue
+++ src/pages/Negocio/CrearCampeonato.vue
@@ -1,59 +1,62 @@
 <template>
     <BarraNav />
-    <br>
-    <h1>Crear Campeonato</h1>
-    <br>
+    <!-- <br> -->
+    <h2>Crear Campeonato</h2>
+    <!-- <br> -->
 
-  <form @submit.prevent="insertar()">
-        <table>
+    <form @submit.prevent="insertar()">
+        <table class="table">
             <tr>
-                <td><label for="">Nombre: </label></td>
+                <td><label for="" id="labelSup">Nombre: </label></td>
                 <td><input class="form-control" v-model="nombre" required type="text"></td>
             </tr>
             <tr>
-                <td><label for="">Federacion que Organiza: </label></td>
+                <td><label for="" id="labelSup">Federación que Organiza: </label></td>
                 <td><input class="form-control" v-model="organizador" required type="text"></td>
             </tr>
             <tr>
-                <td><label for="">Sede: </label></td>
-                <td><input class="form-control" v-model="sede"  type="text" required></td>
+                <td><label for="" id="labelSup">Sede: </label></td>
+                <td><input class="form-control" v-model="sede" type="text" required></td>
             </tr>
             <tr>
-                <td><label for="">Fecha de inicio: </label></td>
+                <td><label for="" id="labelSup">Fecha de inicio: </label></td>
                 <td><input class="form-control" v-model="fInicio" required type="datetime-local"></td>
             </tr>
             <tr>
-                <td><label for="">Fecha de finalizacion</label></td>
+                <td><label for="" id="labelSup">Fecha de finalización</label></td>
                 <td><input class="form-control" v-model="fFin" required type="datetime-local"></td>
             </tr>
             <tr>
-                <td><label for="">Fecha inicio de Inscripcion: </label></td>
+                <td><label for="" id="labelSup">Fecha inicio de inscripción: </label></td>
                 <td><input class="form-control" v-model="finicioI" required type="datetime-local"></td>
             </tr>
             <tr>
-                <td><label for="">Fecha fin de Inscripcion</label></td>
+                <td><label for="" id="labelSup">Fecha fin de inscripción</label></td>
                 <td><input class="form-control" v-model="fFinI" required type="datetime-local"></td>
             </tr>
         </table>
-        <table>            
+
+        <h4>Pruebas</h4>
+
+        <table id="tablaPruebas">
             <tr>
-                <td><label id="prueba" for="">Pruebas: </label></td>
+                <!-- <td><label id="prueba" for="">Pruebas: </label></td> -->
                 <td>
-                <div class="opciones-container">
-                    <div v-for="opcion in listaPruebas" :key="opcion" class="opcion-item">
-                        <input type="checkbox" :id="opcion" :value="opcion.id" v-model="seleccionadas">
-                        <label :for="opcion">{{ opcion.nombre }}</label>
+                    <div class="opciones-container">
+                        <div v-for="opcion in listaPruebas" :key="opcion" class="opcion-item">
+                            <input type="checkbox" :id="opcion" :value="opcion.id" v-model="seleccionadas">
+                            <label :for="opcion">{{ opcion.nombre }}</label>
+                        </div>
                     </div>
-                </div>
                 </td>
             </tr>
-          <br>
-          <table>
-            <tr>
-              <td><button type="submit">Crear Campeonato</button></td>
-            </tr>
-          </table>
-          <br>
+            <br>
+            <table>
+                <tr>
+                    <td><button type="submit" class="btn btn-danger">Crear Campeonato</button></td>
+                </tr>
+            </table>
+            <br>
         </table>
     </form>
     <PiePagina />
@@ -61,78 +64,108 @@
 <script>
 import BarraNav from "@/components/BarraNav.vue";
 import PiePagina from "@/components/PiePagina.vue";
-import {CrearCampeonatoP} from "@/assets/js/campeonato.js"
+import { CrearCampeonatoP } from "@/assets/js/campeonato.js"
 import { listarPruebasFachada } from '@/assets/js/Prueba';
 import router from '@/router';
 
 
 export default {
-    components:{
+    components: {
         BarraNav,
         PiePagina
     },
     data() {
         return {
-            nombre:null,
-            organizador:null,
-            sede:null,
-            fInicio:null,
-            fFin:null,
-            finicioI:null,
-            fFinI:null,
-            listaPruebas:[],
+            nombre: null,
+            organizador: null,
+            sede: null,
+            fInicio: null,
+            fFin: null,
+            finicioI: null,
+            fFinI: null,
+            listaPruebas: [],
             seleccionadas: []
 
         }
     },
 
-methods: {
-    
-    async insertar(){
-        console.log(this.seleccionadas);
-        console.log(sessionStorage.getItem('token'));
-        const campeonato={
-            nombre: this.nombre,
-            organizador: this.organizador,
-            sede: this.sede,
-            fechaInicio: this.fInicio,
-            fechaFin: this.fFin,
-            inscripcionInicio: this.finicioI,
-            inscripcionFin: this.fFinI,
-            pruebas:this.seleccionadas
-        }
+    methods: {
 
-        await CrearCampeonatoP(campeonato);
-        this.nombre = null,
-        this.organizador = null,
-        this.sede = null,
-        this.fInicio = null,
-        this.fFin = null,
-        this.finicioI = null,
-        this.fFinI = null,
-        this.seleccionadas = null
+        async insertar() {
+            console.log(this.seleccionadas);
+            console.log(sessionStorage.getItem('token'));
+            const campeonato = {
+                nombre: this.nombre,
+                organizador: this.organizador,
+                sede: this.sede,
+                fechaInicio: this.fInicio,
+                fechaFin: this.fFin,
+                inscripcionInicio: this.finicioI,
+                inscripcionFin: this.fFinI,
+                pruebas: this.seleccionadas
+            }
 
-        alert("Se ha ingresado correctamente")
-        router.push('/')
+            await CrearCampeonatoP(campeonato);
+            this.nombre = null,
+                this.organizador = null,
+                this.sede = null,
+                this.fInicio = null,
+                this.fFin = null,
+                this.finicioI = null,
+                this.fFinI = null,
+                this.seleccionadas = null
+
+            alert("Se ha ingresado correctamente")
+            router.push('/')
+        },
+        async lista() {
+            this.listaPruebas = await listarPruebasFachada()
+        },
+
+
+
+
     },
-    async lista(){
-        this.listaPruebas = await listarPruebasFachada()
+    mounted() {
+        this.lista()
     },
-
-
-
-
-},
-mounted() {
-    this.lista()
-},
 
 }
 
 
 </script>
+
 <style>
-table{
+#labelSup {
+    font-weight: bold;
+}
+
+
+#tablaPruebas {
+    background-color: #efca08;
+}
+
+
+
+.form-control {
+    border: 2px solid #efca08;
+}
+
+h2 {
+    color: #003153;
+    font-weight: bold;
+    margin-top: 15px;
+    margin-bottom: 15px;
+}
+
+h4 {
+    color: #003153;
+    font-weight: bold;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+table {
     margin: 0 auto;
 }
 
@@ -140,16 +173,17 @@ table{
     display: flex;
     flex-wrap: wrap;
     margin: 30px;
-  }
-  
-  .opcion-item {
-    flex-basis: calc(23.33% - 10px); /* Ajusta el tamaño de cada columna según tus necesidades */
+}
+
+.opcion-item {
+    flex-basis: calc(23.33% - 10px);
+    /* Ajusta el tamaño de cada columna según tus necesidades */
     margin: 5px;
     box-sizing: border-box;
-  }
+}
 
-#prueba{
+/* #prueba{
 
     padding-left: 20px;
-}
+} */
 </style>
