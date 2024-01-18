921e12a468e9ca79fb50107cdd06e1348937e48f
dorisvchb <100151176+dorisvchb@users.noreply.github.com>
2024-01-18 03:56:13 -0500

    Campeonato competidor

---
+++ src/pages/Negocio/CampeonatosCompetidor.vue
@@ -0,0 +1,90 @@
+<template>
+    <!--Barra navegacion-->
+    <BarraNav />
+
+    <div class="container">
+        <h2 class="fw-bold">Campeonatos</h2>
+        <div class="table-responsive-sm">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col" id="encabezadoCamp">Campeonato</th>
+                        <th scope="col" id="encabezadoCamp">Comprobante de pago</th>
+                        <th scope="col" id="encabezadoCamp">Ficha de inscripción</th>
+                        <th scope="col" id="encabezadoCamp">Pago realizado</th>
+                        <th scope="col" id="encabezadoCamp">Ficha de inscripción firmada</th>
+                        <th scope="col" id="encabezadoCamp">Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td><a href="" download>Descargar comprobante</a></td>
+                        <td><a href="" download>Descargar ficha de inscripción</a></td>
+                        <td>
+                            <form enctype="multipart/form-data">
+                                <div>
+                                    <label class="fw-bold" for="comprobantePago">Subir comprobante</label>
+                                    <input type="file" id="comprobantePago">
+                                </div>
+                                <br>
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                                <!-- <input type="submit" value="Enviar" /> -->
+                            </form>
+                        </td>
+                        <td>
+                            <form enctype="multipart/form-data">
+                                <div>
+                                    <label class="fw-bold" for="fichaInscripcion">Subir ficha</label>
+                                    <input type="file" id="fichaInscripcion">
+                                </div>
+                                <br>
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                                <!-- <input type="submit" value="Enviar" /> -->
+                            </form>
+                        </td>
+                        <td> </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <!-- Pie de página -->
+    <PiePagina />
+</template>
+  
+<script>
+import PiePagina from "@/components/PiePagina.vue";
+import BarraNav from "@/components/BarraNav.vue";
+
+
+export default {
+    name: "CampeonatoCompetidor",
+    components: {
+        PiePagina,
+        BarraNav,
+    },
+}
+</script>
+
+<style>
+/* Estilos de Campeonatos competidor */
+#encabezadoCamp {
+    background-color: #52bad1;
+}
+
+.btn-primary.active {
+    background-color: #1a1c1c;
+    /* Color de fondo del botón activo */
+    border-color: #2a2b2e;
+    /*Color de borde del botón activo*/
+}
+
+.btn-primary:not(.active) {
+    background-color: #003153;
+    /* Color de fondo del botón inactivo */
+    border-color: #003153;
+    /*Color de borde del botón inactivo*/
+}
+</style>
--- src/pages/Negocio/FichaInscripcion.vue
+++ src/pages/Negocio/FichaInscripcion.vue
@@ -5,122 +5,130 @@
     <!--Pestañas-->
     <ul class="nav nav-tabs">
       <li class="nav-item">
-        <a class="nav-link" :class="{ active: selectedTab === 'tab1' }" @click="selectedTab = 'tab1'"> 
+        <a class="nav-link" :class="{ active: selectedTab === 'tab1' }" @click="selectedTab = 'tab1'">
           Ficha de Inscripción
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" :class="{ active: selectedTab === 'tab2' }" @click="selectedTab = 'tab2'"> 
+        <a class="nav-link" :class="{ active: selectedTab === 'tab2' }" @click="selectedTab = 'tab2'">
           Confirmación de Inscripción
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" :class="{ active: selectedTab === 'tab3' }" @click="selectedTab = 'tab3'">
+          Campeonatos Competidor
         </a>
       </li>
     </ul>
 
     <!--Ficha de inscripción-->
-    <div v-if="selectedTab ==='tab1'">
+    <div v-if="selectedTab === 'tab1'">
       <br>
       <div class="container" id="ficha1">
         <div class="container" id="encabezado1">
           <h2 align="center" style="color: #fff2f1">Ficha de Inscripción</h2>
         </div>
         <div class="container">
-  
+
           <br>
-          
+
           <form>
             <div class="container" id="tabla1">
               <!--Datos del competidor-->
-            <table class="table-responsive table-striped" >
-              <tr>
+              <table class="table-responsive table-striped">
+                <tr>
                   <td><label for="" id="labelSup">Nombres: </label></td>
                   <td><input class="form-control" v-model="nombres"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">Apellidos: </label></td>
                   <td><input class="form-control" v-model="apellidos"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">Federación: </label></td>
                   <td><input class="form-control" v-model="federacion"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">Dirección: </label></td>
                   <td><input class="form-control" v-model="direccion"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">Ciudad: </label></td>
                   <td><input class="form-control" v-model="ciudad"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">Contacto: </label></td>
                   <td><input class="form-control" v-model="contacto"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">E-mail: </label></td>
                   <td><input class="form-control" v-model="email"></td>
-              </tr>
-              <tr>
+                </tr>
+                <tr>
                   <td><label for="" id="labelSup">Categoría: </label></td>
                   <td><input class="form-control" v-model="categoria"></td>
-              </tr>
-          </table> 
-            </div>   
-                    
-        <br>
-        <!--Selección de campeonato-->
-        <select class="form-select" aria-label="Seleccionar campeonato" id="campeonatos1" style="background-color: #edf3f5; color: #000000;">
-            <option selected>Seleccione un campeonato</option>
-            <option value="1">Enero 2024</option>
-            <option value="2">Febrero 2024</option>
-            <option value="3">Marzo 2024</option>
-          </select>
-        
-        <br>
-        
-        <br>
-        <!--Selección de pruebas-->
-        <div class="container" id="encabezado2">
-          <h5><b>Pruebas</b></h5>
-        </div>
-        <br>
-        <div class="container" id="tabla2">
-          <table class="table-responsive table-hover table-bordered">
-            <thead class="table-dark">
-              <tr>
-                <th scope="col" class="text-dark">Prueba</th>
-                <th scope="col" class="text-dark">Costo</th>
-                <th scope="col" class="text-dark">Seleccionar</th>
-              </tr>
-            </thead>
-            <tbody class="table-group-divider">
-              <tr>
-                <td scope="row">100 metros planos</td>
-                <td>$10</td>
-                <input class="form-check-input" type="checkbox" role="switch">
-              </tr>
-            </tbody>
-          </table>
-        </div>
-          <br>
-          <label>Total a pagar: </label><input class="form-control">
-          <br>
-          <p align="justify">El valor de la inscripción hasta por 3 pruebas individuales es de $15 para socios
-            y de $25 para no socios. Cada prueba individual adicional tendrá un costo de 10 doláres.
-            El Pentatlon no se considera como una prueba individual y tendrá un costo de 10 dólares.
-            Las postas no tendrán costo adicional, pero deben ser enviadas dentro del formato de inscripción,
-            especificando integrantes y categorías. No se permitirá equipos conformados por atletas de varias provincias.																								
-          </p>
-          <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+                </tr>
+              </table>
+            </div>
+
+            <br>
+            <!--Selección de campeonato-->
+            <select class="form-select" aria-label="Seleccionar campeonato" id="campeonatos1"
+              style="background-color: #edf3f5; color: #000000;">
+              <option selected>Seleccione un campeonato</option>
+              <option value="1">Enero 2024</option>
+              <option value="2">Febrero 2024</option>
+              <option value="3">Marzo 2024</option>
+            </select>
+
+            <br>
+
+            <br>
+            <!--Selección de pruebas-->
+            <div class="container" id="encabezado2">
+              <h5><b>Pruebas</b></h5>
+            </div>
+            <br>
+            <div class="container" id="tabla2">
+              <table class="table-responsive table-hover table-bordered">
+                <thead class="table-dark">
+                  <tr>
+                    <th scope="col" class="text-dark">Prueba</th>
+                    <th scope="col" class="text-dark">Costo</th>
+                    <th scope="col" class="text-dark">Seleccionar</th>
+                  </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                  <tr>
+                    <td scope="row">100 metros planos</td>
+                    <td>$10</td>
+                    <input class="form-check-input" type="checkbox" role="switch">
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <br>
+            <label>Total a pagar: </label><input class="form-control">
+            <br>
+            <p align="justify">El valor de la inscripción hasta por 3 pruebas individuales es de $15 para socios
+              y de $25 para no socios. Cada prueba individual adicional tendrá un costo de 10 doláres.
+              El Pentatlon no se considera como una prueba individual y tendrá un costo de 10 dólares.
+              Las postas no tendrán costo adicional, pero deben ser enviadas dentro del formato de inscripción,
+              especificando integrantes y categorías. No se permitirá equipos conformados por atletas de varias
+              provincias.
+            </p>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+          </form>
         </div>
       </div>
     </div>
 
-    <div v-else-if="selectedTab==='tab2'">
+    <!--Verificar Inscripciones-->
+    <div v-else-if="selectedTab === 'tab2'">
       <div class="container" id="ficha2">
         <h2 class="fw-bold">Verificar Inscripciones</h2>
-    
-      <table class="table-responsive table-bordered bordeCaja">
+
+        <table class="table-responsive table-bordered bordeCaja">
           <label for="" id="labelSup">Seleccione campeonato</label>
           <select class="bordeCaja" required>
           </select>
@@ -132,19 +140,70 @@
             <th id="tablaInsc">Aprobar Pago</th>
             <th id="tablaInsc">Aprobar Inscripción</th>
           </tr>
-      </table>
+        </table>
+      </div>
+    </div>
+
+    <!--Campeonatos Competidor-->
+    <div v-else-if="selectedTab === 'tab3'">
+      <div class="container" id="ficha3">
+        <h2 class="fw-bold">Campeonatos</h2>
+        <div class="table-responsive-sm">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col" id="encabezadoCamp">Campeonato</th>
+                <th scope="col" id="encabezadoCamp">Comprobante de pago</th>
+                <th scope="col" id="encabezadoCamp">Ficha de inscripción</th>
+                <th scope="col" id="encabezadoCamp">Pago realizado</th>
+                <th scope="col" id="encabezadoCamp">Ficha de inscripción firmada</th>
+                <th scope="col" id="encabezadoCamp">Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td><a href="" download>Descargar comprobante</a></td>
+                <td><a href="" download>Descargar ficha de inscripción</a></td>
+                <td>
+                  <form enctype="multipart/form-data">
+                    <div>
+                      <label class="fw-bold" for="comprobantePago">Subir comprobante</label>
+                      <input type="file" id="comprobantePago">
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <!-- <input type="submit" value="Enviar" /> -->
+                  </form>
+                </td>
+                <td>
+                  <form enctype="multipart/form-data">
+                    <div>
+                      <label class="fw-bold" for="fichaInscripcion">Subir ficha</label>
+                      <input type="file" id="fichaInscripcion">
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <!-- <input type="submit" value="Enviar" /> -->
+                  </form>
+                </td>
+                <td> </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
     <br>
   </div>
 
-   <!-- Pie de página -->
-   <PiePagina />
+  <!-- Pie de página -->
+  <PiePagina />
 </template>
 
 <script>
-  import PiePagina from "@/components/PiePagina.vue";
-  import BarraNav from "@/components/BarraNav.vue";
+import PiePagina from "@/components/PiePagina.vue";
+import BarraNav from "@/components/BarraNav.vue";
 export default {
   data() {
     return {
@@ -160,9 +219,9 @@ export default {
     };
   },
   components: {
-      PiePagina,
-      BarraNav,
-    },
+    PiePagina,
+    BarraNav,
+  },
 };
 </script>
 
@@ -172,28 +231,37 @@ export default {
   padding: 10px;
 }
 
-#encabezado2{
+#encabezado2 {
   background-color: #52bad1;
   padding: 5px;
 }
+
 .nav-tabs .nav-link {
-  background-color: #6caef5; /* Color de fondo de las pestañas inactivas */
-  color: #2a2b2e; /* Color de texto de las pestañas inactivas */
+  background-color: #6caef5;
+  /* Color de fondo de las pestañas inactivas */
+  color: #2a2b2e;
+  /* Color de texto de las pestañas inactivas */
 }
 
 .nav-tabs .nav-link.active {
-  background-color: #003153; /* Color de fondo de la pestaña activa */
-  color: #ffffff; /* Color de texto de la pestaña activa */
+  background-color: #003153;
+  /* Color de fondo de la pestaña activa */
+  color: #ffffff;
+  /* Color de texto de la pestaña activa */
 }
 
 .btn-primary.active {
-  background-color: #1a1c1c; /* Color de fondo del botón activo */
-  border-color: #2a2b2e; /*Color de borde del botón activo*/
+  background-color: #1a1c1c;
+  /* Color de fondo del botón activo */
+  border-color: #2a2b2e;
+  /*Color de borde del botón activo*/
 }
 
 .btn-primary:not(.active) {
-  background-color: #003153; /* Color de fondo del botón inactivo */
-  border-color: #003153; /*Color de borde del botón inactivo*/
+  background-color: #003153;
+  /* Color de fondo del botón inactivo */
+  border-color: #003153;
+  /*Color de borde del botón inactivo*/
 }
 
 #labelSup {
@@ -201,7 +269,7 @@ export default {
   font-weight: bold;
 }
 
-.form-control{
+.form-control {
   width: 100%;
 }
 
@@ -245,32 +313,35 @@ table {
 }
 
 .bordeCaja {
-border: 2px solid #1a1c1c;
+  border: 2px solid #1a1c1c;
 }
 
-@media (min-width: 768px){
+@media (min-width: 768px) {
   #ficha1 {
     width: 75%
   }
 }
 
-@media (max-width: 767px){
+@media (max-width: 767px) {
   #ficha1 {
     width: 100%;
   }
 }
 
-@media (min-width: 768px){
+@media (min-width: 768px) {
   #ficha2 {
     width: 75%
   }
 }
 
-@media (max-width: 767px){
+@media (max-width: 767px) {
   #ficha2 {
     width: 100%;
   }
 }
 
-
+/* Estilos de Campeonatos competidor */
+#encabezadoCamp {
+  background-color: #52bad1;
+}
 </style>
