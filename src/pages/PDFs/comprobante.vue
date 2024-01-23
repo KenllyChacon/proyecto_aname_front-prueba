2e3a3cd0db95234d6d3a419444cbeaae57beed48
sterk55 <daniel49571@gmail.com>
2024-01-23 02:04:29 -0500

    2

--- src/pages/PDFs/comprobante.vue
+++ src/pages/PDFs/comprobante.vue
@@ -1,143 +1,145 @@
 
 <template>
-    <div class="factura">
-      <header>
-        <h1>FACTURA</h1>
-      </header>
-      <main>
-        <section class="datos-cliente">
-          <h3>Cliente</h3>
-          <ul>
-            <li>Nombre: {{nombreCompetidor}}</li>
-            <li>Email: {{ emailCompetidor }}</li>
-          </ul>
-        </section>
-        <section class="items">
-          <table>
-            <thead>
+  <div class="factura">
+    <header>
+      <h1>FACTURA</h1>
+    </header>
+    <main>
+      <section class="datos-cliente">
+        <h3>Cliente</h3>
+        <ul>
+          <li>Nombre: {{ nombreCompetidor }}</li>
+          <li>Email: {{ emailCompetidor }}</li>
+        </ul>
+      </section>
+      <section class="items">
+        <table>
+          <thead>
             <tr>
               <th>PRUEBAS</th>
               <th>PRECIO</th>
             </tr>
-            </thead>
-            <tbody>
+          </thead>
+          <tbody>
             <tr v-for="l in pruebas3" :key="l">
-                <td>{{l.nombre }}</td>
-                <td><p>$5</p></td>
+              <td>{{ l.nombre }}</td>
+              <td><p>$5</p></td>
             </tr>
             <tr v-for="li in pruebasRes" :key="li">
-                <td>{{ li.nombre }}</td>
-                <td><p>$10</p></td>
+              <td>{{ li.nombre }}</td>
+              <td><p>$10</p></td>
             </tr>
-            </tbody>
-          </table>
-        </section>
-        <section class="totales">
-          <ul>
-            <li>TOTAL: {{total}}</li>
-          </ul>
-        </section>
-        <section class="informacion-pago">
-          <h3>Información para el pago</h3>
-          <ul>
-            <li>
-              <label for="banco">Institución financiera: Banco Pichincha</label>
-            </li>
-            <li>
-              <label for="titular-cuenta">Nombre del titular: Cristhian Cedeño</label>
-            </li>
-            <li>
-              <label for="numero-cuenta">Número de cuenta: 2204446646</label>
-            </li>
-          </ul>
-        </section>
-      </main>
-    </div>
-
+          </tbody>
+        </table>
+      </section>
+      <section class="totales">
+        <ul>
+          <li>TOTAL: {{ total }}</li>
+        </ul>
+      </section>
+      <section class="informacion-pago">
+        <h3>Información para el pago</h3>
+        <ul>
+          <li>
+            <label for="banco">Institución financiera: Banco Pichincha</label>
+          </li>
+          <li>
+            <label for="titular-cuenta"
+              >Nombre del titular: Cristhian Cedeño</label
+            >
+          </li>
+          <li>
+            <label for="numero-cuenta">Número de cuenta: 2204446646</label>
+          </li>
+        </ul>
+      </section>
+    </main>
+  </div>
 </template>
 
 <script>
-import jsPDF from 'jspdf'
-import { campIncritosUsersP, campIncritosUserEmailP } from '@/assets/js/campeonato'
+import jsPDF from "jspdf";
+import {
+  campIncritosUsersP,
+  campIncritosUserEmailP,
+} from "@/assets/js/campeonato";
 
-export default{
-
-    data(){
-
-        return{
-            nombreCompetidor: null,
-            emailCompetidor: sessionStorage.getItem('email'),
-            pruebas3:[],
-            pruebasRes:[],
-            total:null,
-            listaCompetidor: [],
-            listaCampInscritosUserEmail: [],
-            listaPruebas: []
-
-
-
-        }
-
+export default {
+  data() {
+    return {
+      nombreCompetidor: null,
+      emailCompetidor: sessionStorage.getItem("email"),
+      pruebas3: [],
+      pruebasRes: [],
+      total: null,
+      listaCompetidor: [],
+      listaCampInscritosUserEmail: [],
+      listaPruebas: [],
+    };
+  },
+  props: {
+    idCampeonato: {
+      type: String,
+      required: true,
     },
-    props:{
-        idCampeonato:{
-            type: String,
-            required: true
-        }
-    },
-    methods:{
-        descargar(){
-            this.asignarValores()
-            var doc = new jsPDF('p', 'pt', 'A4')
-            var margins = 0;
-            var scale = (doc.internal.pageSize.width - margins * 2) / document.body.scrollWidth;
-            doc.html(this.$refs.content, {
-                x: margins,
-                y: margins,
-                html2canvas: {
-                    scale: scale,
-                },
-                callback: function (doc) {
-                    doc.output('dataurlnewwindow', { filename: 'fichero-pdf.pdf' })
-                }
-            })
-
-            doc.fromHTML(this.$refs.content, this.margins.left, this.margins.top, {
-                'width': this.margins.width
-            });
-            doc.save('Comprobante.pdf')
-
+  },
+  methods: {
+    descargar() {
+      this.asignarValores();
+      var doc = new jsPDF("p", "pt", "A4");
+      var margins = 0;
+      var scale =
+        (doc.internal.pageSize.width - margins * 2) / document.body.scrollWidth;
+      doc.html(this.$refs.content, {
+        x: margins,
+        y: margins,
+        html2canvas: {
+          scale: scale,
         },
-        async asignarValores(){
-            this.listaCompetidor = await campIncritosUsersP(sessionStorage.getItem('email'))
-            this.listaCampInscritosUserEmail = await campIncritosUserEmailP(sessionStorage.getItem("email"))
-            this.nombreCompetidor = this.listaCompetidor[0].nombres
-            this.listaPruebas = this.listaCampInscritosUserEmail[0].pruebas
-            console.log(this.listaPruebas);
+        callback: function (doc) {
+          doc.output("dataurlnewwindow", { filename: "fichero-pdf.pdf" });
         },
-        cerrar(){
-            window.close()
-        },
-        random(){
-            if(this.listaPruebas.length === 3){
-                this.pruebas3 = this.listaPruebas.slice(0,3)
-            }else{
-                this.pruebas3 = this.listaPruebas.slice(0,3)
-                this.pruebasRes = this.listaPruebas.slice(3)
-            }
-        }
+      });
+
+      doc.fromHTML(this.$refs.content, this.margins.left, this.margins.top, {
+        width: this.margins.width,
+      });
+      doc.save("Comprobante.pdf");
     },
-    async mounted() {
-        //setTimeout(this.cerrar, 1560)
-        //this.descargar()
-        this.asignarValores()
-        this.random()
+    async asignarValores() {
+      this.listaCompetidor = await campIncritosUsersP(
+        sessionStorage.getItem("email")
+      );
+      this.listaCampInscritosUserEmail = await campIncritosUserEmailP(
+        sessionStorage.getItem("email")
+      );
+      this.nombreCompetidor = this.listaCompetidor[0].nombres;
+      this.listaPruebas = this.listaCampInscritosUserEmail[0].pruebas;
+      console.log(this.listaPruebas);
+      this.random();
+      console.log("Lista 3: " + this.pruebas3.length);
     },
-    
-}
-
-
-
+    cerrar() {
+      window.close();
+    },
+    random() {
+      // Asegurarse de que pruebas3 esté definido incluso si listaPruebas tiene menos de 3 elementos
+      this.pruebas3 = this.listaPruebas.slice(0, 3);
+      // Si hay más de 3 elementos, asignar el resto a pruebasRes
+      if (this.listaPruebas.length > 3) {
+        this.pruebasRes = this.listaPruebas.slice(3);
+      } else {
+        // Si listaPruebas tiene 3 o menos elementos, asegurarse de que pruebasRes esté vacío
+        this.pruebasRes = [];
+      }
+    },
+  },
+  async mounted() {
+    //setTimeout(this.cerrar, 1560)
+    //this.descargar()
+    this.asignarValores();
+  },
+};
 </script>
 
 
