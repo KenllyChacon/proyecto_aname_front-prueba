c51da459216a26ecc97e1347cb300f73611a9c01
dorisvchb <100151176+dorisvchb@users.noreply.github.com>
2024-02-25 10:48:03 -0500

    eliminar 3 arch AboutView, HomeView, Hello World

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,7 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "Inicio" */ "@/pages/Negocio/Inicio.vue"),
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "AboutView" */ "../views/AboutView.vue"),
-  },
+ 
   {
     path: "/login",
     name: "login",
