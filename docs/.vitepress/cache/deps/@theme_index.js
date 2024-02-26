b2088f5396c4bb890cb86321058ee31eb012f0fd
saguaminga <saralej95@gmail.com>
2024-02-25 23:05:07 -0500

    Caché doc

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,32 +9,32 @@ import {
 } from "./chunk-RBH54IHM.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default6 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default7 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default8 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default9 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default10 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default11 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default12 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default13 } from "D:/ProyectosGithubClonados/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default6 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default7 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default8 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default9 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default10 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default11 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default12 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default13 } from "D:/UCE/Grid and Cloud/ANAME/proyecto_aname_front-prueba/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/shared.js
 var inBrowser = typeof document !== "undefined";
